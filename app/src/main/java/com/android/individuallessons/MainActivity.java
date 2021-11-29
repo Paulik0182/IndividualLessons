e0cd808e65aa613db9ed27420e0a8a93d0fb0df9
Paveł <Paulik0182@gmail.com>
2021-11-29 11:20:09 +0300

    Activity -> создано два активити, ресурсы.

--- app/src/main/java/com/android/individuallessons/MainActivity.java
+++ app/src/main/java/com/android/individuallessons/MainActivity.java
@@ -1,14 +1,19 @@
 package com.android.individuallessons;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.individuallessons.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding = null;// переменная определяющая все элементы в Activity через класс ActivityMainBinding
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
+        binding = ActivityMainBinding.inflate ( getLayoutInflater () ); // наполняем binding, обращаемся к методу который из Activity берет макет
         setContentView ( R.layout.activity_main );
     }
 }
