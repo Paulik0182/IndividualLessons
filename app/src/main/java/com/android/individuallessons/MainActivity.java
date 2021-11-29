f84c43f005b501c64efacb36012c2af92483b9fd
Paveł <Paulik0182@gmail.com>
2021-11-29 15:56:35 +0300

    Class -> SecondActivity, Counter. Создано два класса. Иницилизирована кнопка перехода во второй Activity, но не получилось реализовать binding.

--- app/src/main/java/com/android/individuallessons/MainActivity.java
+++ app/src/main/java/com/android/individuallessons/MainActivity.java
@@ -1,19 +1,87 @@
 package com.android.individuallessons;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.android.individuallessons.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding = null;// переменная определяющая все элементы в Activity через класс ActivityMainBinding
+    private static final String TAG = "@@@MainActivity";
+
+
+    private Button buttonActivity = null;
+
+//    private ActivityMainBinding binding = null;// переменная определяющая все элементы в Activity через класс ActivityMainBinding
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
-        binding = ActivityMainBinding.inflate ( getLayoutInflater () ); // наполняем binding, обращаемся к методу который из Activity берет макет
+//        binding = ActivityMainBinding.inflate ( getLayoutInflater () ); // наполняем binding, обращаемся к методу который из Activity берет макет
+//        binding= DataBindingUtil.setContentView ( this, R.layout.activity_main );
         setContentView ( R.layout.activity_main );
+        Log.d ( TAG, "onCreate() called with: savedInstanceState = [" + savedInstanceState + "]" );
+
+        buttonActivity = findViewById ( R.id.button_activity2 );
+
+        findViewById ( R.id.button_click ).setOnClickListener ( v -> {
+            Toast.makeText ( this, R.string.click, Toast.LENGTH_SHORT ).show ();
+            //todo
+        } );
+
+        buttonActivity.setOnClickListener ( v -> {
+            Log.d ( TAG, "MainActivity -> SecondActivity" );
+            Toast.makeText ( this, R.string.button_activity_2, Toast.LENGTH_SHORT ).show ();
+
+            Intent intent = new Intent ( MainActivity.this, SecondActivity.class );
+            startActivity ( intent );
+        } );
+
+//        binding.buttonClick.setOnClickListener ( v -> {
+//            Toast.makeText ( this, R.string.click, Toast.LENGTH_SHORT ).show ();
+//            //todo
+//        } );
+//
+//        binding.buttonActivity2.setOnClickListener ( v -> {
+//            Log.d ( TAG, "MainActivity -> SecondActivity" );
+//            Toast.makeText ( this, R.string.button_activity_2, Toast.LENGTH_SHORT ).show ();
+//
+//            Intent intent = new Intent ( MainActivity.this, SecondActivity.class );
+//            startActivity ( intent );
+//        } );
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart ();
+        Log.d ( TAG, "onStart() called" );
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume ();
+        Log.d ( TAG, "onResume() called" );
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause ();
+        Log.d ( TAG, "onPause() called" );
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop ();
+        Log.d ( TAG, "onStop() called" );
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d ( TAG, "onDestroy() called" );
+        super.onDestroy ();
     }
 }
---
+++ app/src/main/java/com/android/individuallessons/SecondActivity.java
@@ -0,0 +1,59 @@
+package com.android.individuallessons;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.individuallessons.databinding.ActivitySecondBinding;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private static final String TAG = "@@@SecondActivity";
+
+    private ActivitySecondBinding binding = null;// переменная определяющая все элементы в Activity через класс ActivityMainBinding
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
+        binding = ActivitySecondBinding.inflate ( getLayoutInflater () ); // наполняем binding, обращаемся к методу который из Activity берет макет
+        setContentView ( R.layout.activity_second );
+        Log.d ( TAG, "SecondActivity onCreate() called with: savedInstanceState = [" + savedInstanceState + "]" );
+
+        binding.buttonBack.setOnClickListener ( v -> {
+            Toast.makeText ( this, R.string.activity_1, Toast.LENGTH_SHORT ).show ();
+
+        } );
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart ();
+        Log.d ( TAG, "onStart() SecondActivity" );
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume ();
+        Log.d ( TAG, "onResume() SecondActivity" );
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause ();
+        Log.d ( TAG, "onPause() SecondActivity" );
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop ();
+        Log.d ( TAG, "onStop() SecondActivity" );
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d ( TAG, "onDestroy() SecondActivity" );
+        super.onDestroy ();
+    }
+}
